f0caff50dca304283e0d0dbb1c636433688591a7
Gaeon <gaeon0413@gmail.com>
2024-04-02 19:06:05 +0900

    django 기본구조 작성

--- financial_log/financial_log/settings.py
+++ financial_log/financial_log/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'calender_app',
+    'wallet_app',
+    'diary_app'
 ]
 
 MIDDLEWARE = [
